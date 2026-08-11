fc80c29875255cbc490376047ab3c80ee042470d
hegezotya-cmyk <hegezotya@gmail.com>
2026-08-11 03:21:10 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 export default {
   async fetch(request, env) {
+    const url = new URL(request.url);
+
+    if (url.pathname === "/") {
+      return env.ASSETS.fetch(
+        new Request(new URL("/index.html", request.url))
+      );
+    }
+
     return env.ASSETS.fetch(request);
   },
 };
