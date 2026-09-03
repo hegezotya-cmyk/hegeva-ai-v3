2c62f173fa95288bd5c666ee22ab548daaa688fc
hegezotya-cmyk <hegezotya@gmail.com>
2026-09-04 00:12:40 +0100

    Add campaign approval workflow

--- v0-app/app/app-studio/advertising/page.tsx
+++ v0-app/app/app-studio/advertising/page.tsx
@@ -1,5 +1,6 @@
 import { AdvertisingStudio } from "@/components/app-studio/advertising-studio"
+import { CampaignApprovalBoard } from "@/components/app-studio/campaign-approval-board"
 
 export default function AdvertisingPage() {
-  return <AdvertisingStudio />
+  return <><AdvertisingStudio /><div className="mx-auto -mt-8 max-w-7xl px-4 pb-12 sm:px-6 lg:px-8"><CampaignApprovalBoard /></div></>
 }
