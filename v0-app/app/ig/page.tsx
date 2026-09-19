09a0983865be03cf4cebd00d2270ba2d0bb368a6
hegezotya-cmyk <hegezotya@gmail.com>
2026-09-19 04:33:48 +0100

    Add /ig tracked campaign short link

---
+++ v0-app/app/ig/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from "next/navigation"
+
+export default function CampaignRedirect() {
+  redirect("/?utm_source=instagram&utm_medium=organic&utm_campaign=hegeva_growth_2026")
+}
