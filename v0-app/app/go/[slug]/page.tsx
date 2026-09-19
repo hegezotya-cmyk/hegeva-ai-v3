3204bfd26dc2d47dcf01168a84c8ffd7331a76c6
hegezotya-cmyk <hegezotya@gmail.com>
2026-09-19 05:31:17 +0100

    Add measurable Distribution V2 short links

---
+++ v0-app/app/go/[slug]/page.tsx
@@ -0,0 +1,26 @@
+import { redirect } from "next/navigation"
+
+type DistributionPageProps = {
+  params: Promise<{ slug: string }>
+}
+
+const routes: Record<string, string> = {
+  "li-challenge": "/challenge?utm_source=linkedin&utm_medium=organic&utm_campaign=hegeva_growth_2026&utm_content=v2_challenge",
+  "li-consultants": "/for-consultants?utm_source=linkedin&utm_medium=organic&utm_campaign=hegeva_growth_2026&utm_content=v2_consultants",
+  "fb-small-business": "/free-tools?utm_source=facebook&utm_medium=organic&utm_campaign=hegeva_growth_2026&utm_content=v2_free_tools",
+  "fb-electricians": "/for-electricians?utm_source=facebook&utm_medium=organic&utm_campaign=hegeva_growth_2026&utm_content=v2_electricians",
+  "fb-builders": "/for-builders?utm_source=facebook&utm_medium=organic&utm_campaign=hegeva_growth_2026&utm_content=v2_builders",
+  "fb-plumbers": "/for-plumbers?utm_source=facebook&utm_medium=organic&utm_campaign=hegeva_growth_2026&utm_content=v2_plumbers",
+  "fb-cleaners": "/for-cleaners?utm_source=facebook&utm_medium=organic&utm_campaign=hegeva_growth_2026&utm_content=v2_cleaners",
+  "fb-property": "/for-property-maintenance?utm_source=facebook&utm_medium=organic&utm_campaign=hegeva_growth_2026&utm_content=v2_property",
+  "ig-challenge": "/challenge?utm_source=instagram&utm_medium=organic&utm_campaign=hegeva_growth_2026&utm_content=v2_challenge",
+  "tt-challenge": "/challenge?utm_source=tiktok&utm_medium=organic&utm_campaign=hegeva_growth_2026&utm_content=v2_challenge",
+  "yt-challenge": "/challenge?utm_source=youtube&utm_medium=organic&utm_campaign=hegeva_growth_2026&utm_content=v2_challenge",
+  "sc-challenge": "/challenge?utm_source=snapchat&utm_medium=organic&utm_campaign=hegeva_growth_2026&utm_content=v2_challenge",
+  "reddit-free-tools": "/free-tools?utm_source=reddit&utm_medium=organic&utm_campaign=hegeva_growth_2026&utm_content=v2_free_tools",
+}
+
+export default async function DistributionRedirect({ params }: DistributionPageProps) {
+  const { slug } = await params
+  redirect(routes[slug] || "/challenge")
+}
